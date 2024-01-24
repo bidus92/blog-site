2702869596dc182c3ad2a23bc68bc8e86978310e
bidus92 <bidusblogs@gmail.com>
2024-01-24 15:48:22 -0500

    FINISHED! (for now) comment sections on all pages function as they should (granted no knowledge of databases at this time but will implement) added  comment_section.ejs to put to all review pages and a global filepath variable that changes upon which page you access so that comments function accordingly.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser";
 import {fileURLToPath} from "url";
 import {dirname} from "path";
 import ejs from "ejs";
-
+import path from "path-locator"; //key
 
 //initializes express framework within our Node Project
 const app = express(); 
-
+ 
 //tells express where to find our static files 
 app.use(express.static("public")); 
 
@@ -19,6 +19,13 @@ const port = 3000;
 //...to the ___dirname function that stores the path 
 const ___dirname = dirname(fileURLToPath(import.meta.url));
 
+var theFilepath; 
+
+function getFilepath(file)
+{
+    return theFilepath = path.locate(file);
+}
+
 //variable to substitute the body parser middleware to allow for parsing of site info
 const theBody = bodyParser.urlencoded({extended: true});
 
@@ -28,6 +35,8 @@ app.use(theBody);
 //GETS
 app.get("/", (req, res)=>
 {
+    theFilepath = getFilepath("index.ejs"); 
+    console.log(theFilepath); 
     res.render(___dirname + "/views/partials/index.ejs");
 });
 
@@ -40,13 +49,12 @@ app.get("/portfolio", (req, res)=>
 
 app.get("/submit", (req, res)=>
 {
+    theFilepath = getFilepath("submit.ejs"); 
+    console.log(theFilepath); 
     res.render(___dirname + "/views/partials/submit.ejs");
 });
 
-app.post("/submit", (req, res)=>
-{
-   res.render("/views/partials/submit.ejs"); 
-});
+
 
 app.get("/reviews", (req, res)=>
 {
@@ -55,25 +63,44 @@ app.get("/reviews", (req, res)=>
 
 app.get("/reviews/f13-part-4", (req, res)=>
 {
+    theFilepath = getFilepath("f13.ejs"); 
+    console.log(theFilepath); 
     res.render(___dirname + "/views/partials/reviews/f13.ejs");
 });
 
 app.get("/reviews/batman-forever", (req, res)=>
 {
-    res.render(___dirname + "/views/partials/reviews/batman.ejs");
+    theFilepath = getFilepath("batman.ejs"); 
+    console.log(theFilepath); 
+    res.render( ___dirname + "/views/partials/reviews/batman.ejs");
 });
 
 app.get("/reviews/rots", (req, res)=>
 {
+    theFilepath = getFilepath("rots.ejs"); 
+    console.log(theFilepath); 
     res.render(___dirname + "/views/partials/reviews/rots.ejs");
 });
 
 app.get("/reviews/goldeneye", (req, res)=>
 {
+    theFilepath = getFilepath("goldeneye.ejs"); 
+    console.log(theFilepath); 
     res.render(___dirname + "/views/partials/reviews/goldeneye.ejs");
 });
 
+app.post("/submit", (req, res)=>
+{
+     var title = req.body["postTitle"];
+     var reviews = req.body["reviewerPost"];
+     console.log(theFilepath); 
+        res.render(theFilepath,
+        {
+            title: title,
+            reviews: reviews
+        });  
 
+});
 
 //assign our port to the server to handle this node app/website; 
 app.listen(port, ()=>
